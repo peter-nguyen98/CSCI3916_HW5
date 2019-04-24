6ace1c432acd30f7269886fa803986b6a937cad8
peter-nguyen98 <46980992+peter-nguyen98@users.noreply.github.com>
2019-04-24 11:50:50 -0600

    Final commit

--- src/actions/reviewActions.js
+++ src/actions/reviewActions.js
@@ -1,67 +1,54 @@
 import actionTypes from '../constants/actionTypes';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
-
-function moviesFetched(movies){
-    return {
-        type: actionTypes.FETCH_MOVIES,
-        movies: movies
-    }
-}
-
-function movieFetched(movie){
-    return {
-        type: actionTypes.FETCH_MOVIE,
-        selectedMovie: movie
-    }
-}
-
-function movieSet(movie){
-    return {
-        type: actionTypes.SET_MOVIE,
-        selectedMovie: movie
-    }
-}
-
-export function setMovie(movie) {
-    return dispatch => {
-        dispatch(movieSet(movie));
-    }
-}
-
-export function fetchMovies(){
+/*
+export function submitReview(review){
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/movies?review=true`, {
-            method: 'GET',
+        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+            method: 'POST',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('token')
+                'Content-Type': 'application/json'
             },
+            body: JSON.stringify(review),
             mode: 'cors'})
             .then( (response) => {
                 if (!response.ok) {
                     throw Error(response.statusText);
                 }
                 return response.json();
-            })
-            .then( (res) => {
-                dispatch(moviesFetched(res));
             })
             .catch( (e) => console.log(e) );
     }
+}*/
+
+function reviewPosted(review){
+    return {
+        type: actionTypes.POST_REVIEW,
+        review: review
+    }
 }
 
-export function fetchMovie(movieId){
+export function submitReview(review){
     const env = runtimeEnv();
+	var formBody = [];
+	for(var property in review)
+	{
+		var encodedKey = encodeURIComponent(property);
+		var encodedValue = encodeURIComponent(review[property]);
+		formBody.push(encodedKey + "=" + encodedValue);
+	}
+	formBody = formBody.join("&");
+	
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/movies/${movieId}?review=true`, {
-            method: 'GET',
+        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+            method: 'POST',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
                 'Authorization': localStorage.getItem('token')
             },
+			body: formBody,
             mode: 'cors'})
             .then( (response) => {
                 if (!response.ok) {
@@ -70,7 +57,7 @@ export function fetchMovie(movieId){
                 return response.json();
             })
             .then( (res) => {
-                dispatch(movieFetched(res));
+                dispatch(reviewPosted(res));
             })
             .catch( (e) => console.log(e) );
     }
